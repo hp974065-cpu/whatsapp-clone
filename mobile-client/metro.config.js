ab45f171ad368ef0f62c59ac6a798bf6fd68998b
haripriya <hp974065@gmail.com>
2026-02-24 17:12:53 +0530

    Deployment: Fix web styling by adding PostCSS and correcting configs

--- mobile-client/metro.config.js
+++ mobile-client/metro.config.js
@@ -1,13 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
-let withNativeWind;
-try {
-    withNativeWind = require("nativewind/metro-config").withNativeWind;
-} catch (e) {
-    console.warn("NativeWind metro-config failed to load, falling back to default.");
-}
+const { withNativeWind } = require("nativewind/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind
-    ? withNativeWind(config, { input: "./global.css" })
-    : config;
+module.exports = withNativeWind(config, { input: "./global.css" });
